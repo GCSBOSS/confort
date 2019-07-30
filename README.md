9c9268c0ac8e258358bfcf684d39e2c48576abab
Guilherme C. Souza <gcsgacc@gmail.com>
2019-07-30 12:54:48 -0300

    Add files for initial version

---
+++ README.md
@@ -0,0 +1,65 @@
+# [Confort](https://gitlab.com/GCSBOSS/confort)
+
+A library for incrementaly build config objects through layering config files in many formats.
+Check out the features:
+
+- Support native objects directly
+- Load config files
+- Merge configs chronologically
+- Option to watch changes in loaded config files
+- Supported formats so far: TOML, JSON and YAML
+
+## Get Started
+
+Install with: `npm i -P confort`
+
+In your code:
+
+```js
+const Confort = require('confort');
+
+// Constructor Forms
+let conf = new Confort(); // Empty conf object
+let conf = new Confort({ key: 'value', key2: 'value2' }); // Initial conf from objects
+let conf = new Confort('./my-file.toml'); // Initial conf from file
+
+// Adding incremental config layer
+conf.addLayer({ key: 'value' });
+conf.object; // => { key: 'value' }
+conf.addLayer({ key: 'newValue', otherKey: 'value' });
+conf.object; // => { key: 'newValue', otherKey: 'value' }
+conf.addLayer('./my-file.yml');
+
+// Reset the configuration for reuse
+conf.clear();
+
+// Reset the conf and reapply all layers effectively reading changes in files
+conf.reload();
+
+// Enable auto reload when changes in loaded conf files happen
+conf.liveReload = true;
+
+// Disables auto reload
+conf.liveReload = false;
+```
+
+## Reporting Bugs
+If you have found any problems with this module, please:
+
+1. [Open an issue](https://gitlab.com/GCSBOSS/confort/issues/new).
+2. Describe what happened and how.
+3. Also in the issue text, reference the label `~bug`.
+
+We will make sure to take a look when time allows us.
+
+## Proposing Features
+If you wish to get that awesome feature or have some advice for us, please:
+1. [Open an issue](https://gitlab.com/GCSBOSS/confort/issues/new).
+2. Describe your ideas.
+3. Also in the issue text, reference the label `~proposal`.
+
+## Contributing
+If you have spotted any enhancements to be made and is willing to get your hands
+dirty about it, fork us and
+[submit your merge request](https://gitlab.com/GCSBOSS/confort/merge_requests/new)
+so we can collaborate effectively.
---
+++ test/spec.js
@@ -0,0 +1,166 @@
+const fs = require('fs');
+const assert = require('assert');
+
+describe('Conf Loader', () => {
+    const loadConf = require('../lib/conf-loader');
+
+    it('Should fail if no conf file is specified', () => {
+        assert.throws( () => loadConf() );
+    });
+
+    it('Should fail if conf file is not found', () => {
+        assert.throws( () => loadConf('./bla.json', /couldn't open/) );
+    });
+
+    it('Should fail if given conf type is not supported', () => {
+        assert.throws( () => loadConf('./test/res/conf.xml'), /type not supported/ );
+    });
+
+    it('Should properly load a TOML file and generate an object', () => {
+        let obj = loadConf('./test/res/conf.toml');
+        assert.strictEqual(obj.key, 'value');
+    });
+
+    it('Should properly load an YAML file and generate an object', () => {
+        let obj = loadConf('./test/res/conf.yaml');
+        assert.strictEqual(obj.key, 'value');
+    });
+
+    it('Should properly load an JSON file and generate an object', () => {
+        let obj = loadConf('./test/res/conf.json');
+        assert.strictEqual(obj.key, 'value');
+    });
+});
+
+describe('Confort', function(){
+    const Confort = require('../lib/main.js');
+
+    describe('constructor ( pathOrObject, [fileType] )', () => {
+
+        it('Should add layers if specified', () => {
+            let conf = new Confort({ key: 'value' });
+            assert.strictEqual(conf.object.key, 'value');
+        });
+
+    });
+
+    describe('::addLayer ( pathOrObject, [fileType] )', () => {
+
+        it('Should add object layer', () => {
+            let conf = new Confort();
+            conf.addLayer({ key: 'value' });
+            assert.strictEqual(conf.object.key, 'value');
+        });
+
+        it('Should override layer values also present in previous state', () => {
+            let conf = new Confort({ key: 'value' });
+            conf.addLayer({ key: 'valu3' });
+            assert.strictEqual(conf.object.key, 'valu3');
+        });
+
+        it('Should preserve values not present in new layer', () => {
+            let conf = new Confort({ key: 'value' });
+            conf.addLayer({ newKey: 'valu3' });
+            assert.strictEqual(conf.object.key, 'value');
+        });
+
+        it('Should load conf file when path is specified', () => {
+            let conf = new Confort('./test/res/conf.toml');
+            assert.strictEqual(conf.object.key, 'value');
+        });
+
+        it('Should record layers info', () => {
+            let conf = new Confort('./test/res/conf.toml');
+            conf.addLayer({ newKey: 'valu3' });
+            assert.strictEqual(conf.layers[0][0], './test/res/conf.toml');
+            assert.strictEqual(conf.layers[1][0].newKey, 'valu3');
+            assert.strictEqual(conf.files[0], './test/res/conf.toml');
+        });
+
+        it('Should emit layer event', done => {
+            let conf = new Confort('./test/res/conf.toml');
+            conf.on('layer', done);
+            conf.addLayer({ newKey: 'valu3' });
+        });
+
+        it('Should add new files to watch list if watching', function(done){
+            fs.copyFileSync('./test/res/conf.toml', './node_modules/conf.toml');
+            let conf = new Confort();
+            conf.liveReload = true;
+            conf.addLayer('./node_modules/conf.toml');
+            conf.on('reload', () => {
+                conf.liveReload = false;
+                done();
+            });
+            fs.writeFileSync('./node_modules/conf.toml', 'key = \'value2\'');
+        });
+
+    });
+
+    describe('::clear ( )', () => {
+
+        it('Should remove all records and clear state', () => {
+            let conf = new Confort('./test/res/conf.toml');
+            conf.addLayer({ newKey: 'valu3' });
+            conf.clear();
+            assert.strictEqual(conf.layers.length, 0);
+            assert.strictEqual(conf.files.length, 0);
+            assert.strictEqual(Object.keys(conf.object).length, 0);
+        });
+
+    });
+
+    describe('::reload ( )', () => {
+
+        it('Should emit reload event', done => {
+            let conf = new Confort('./test/res/conf.toml');
+            conf.on('reload', done);
+            conf.reload();
+        });
+
+        it('Should maintain state unchanged when no file changed', () => {
+            let conf = new Confort('./test/res/conf.toml');
+            conf.addLayer({ newKey: 'valu3' });
+            conf.reload();
+            assert.strictEqual(conf.layers[0][0], './test/res/conf.toml');
+            assert.strictEqual(conf.layers[1][0].newKey, 'valu3');
+            assert.strictEqual(conf.files[0], './test/res/conf.toml');
+            assert.strictEqual(conf.object.key, 'value');
+        });
+
+        it('Should load any values changed in recorded files', () => {
+            fs.copyFileSync('./test/res/conf.toml', './node_modules/conf.toml');
+            let conf = new Confort('./node_modules/conf.toml');
+            conf.addLayer({ newKey: 'valu3' });
+            fs.writeFileSync('./node_modules/conf.toml', 'key = \'value2\'');
+            conf.reload();
+            assert.strictEqual(conf.object.key, 'value2');
+        });
+
+    });
+
+    describe('::liveReload ( enabled )', () => {
+
+        it('Should reload on conf file changes', function(done){
+            fs.copyFileSync('./test/res/conf.toml', './node_modules/conf.toml');
+            let conf = new Confort('./node_modules/conf.toml');
+            conf.liveReload = true;
+            conf.liveReload = true;
+            fs.writeFileSync('./node_modules/conf.toml', 'key = \'value2\'');
+            conf.on('reload', () => {
+                assert.strictEqual(conf.object.key, 'value2');
+                conf.liveReload = false;
+                done();
+            });
+        });
+
+        it('Should noop when enabling already enabled', function(){
+            let conf = new Confort();
+            conf.liveReload = true;
+            conf.liveReload = true;
+            conf.liveReload = false;
+        });
+
+    });
+
+});
